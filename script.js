d2125ddf2aa9b72d2dd5e4f8898bd5c8a080757e
Julien <julien.ramos@epitech.eu>
2020-05-13 05:54:04 +0200

    step 2 input with display done

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function evennumber() {
     var first = parseInt(document.getElementById("number1").value);
     var second = parseInt(document.getElementById("number2").value);
     if (first > 100 || first < 2) {
-        document.getElementById('resultarea').innerHTML = "Invalid input, please try again.";
+        document.getElementById('resultarea').innerHTML = `Number 1 input ${first} is not a valid number`;
     }
     if (second > 100 || second < 2) {
-        document.getElementById('resultarea').innerHTML = "Invalid input, please try again.";
+        document.getElementById('resultarea').innerHTML = `Number 2 input ${second} is not a valid number`;
     }
 }

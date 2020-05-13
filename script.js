dace2215c63a74eb27a21658abbb1f6a259e8840
Julien <julien.ramos@epitech.eu>
2020-05-13 06:03:58 +0200

    step 3 check lower/max input

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function evennumber() {
     var first = parseInt(document.getElementById("number1").value);
     var second = parseInt(document.getElementById("number2").value);
+    var error = 0;
     if (first > 100 || first < 2) {
         document.getElementById('resultarea').innerHTML = `Number 1 input ${first} is not a valid number`;
+        error = 1;
     }
     if (second > 100 || second < 2) {
         document.getElementById('resultarea').innerHTML = `Number 2 input ${second} is not a valid number`;
+        error = 1;
+    }
+    if (error == 0){
+        var min, max;
+        var answer = [];
+        if (first < second) {
+            min = first;
+            max = second;
+        } else {
+            min = second;
+            max = first;
+        }
     }
 }
